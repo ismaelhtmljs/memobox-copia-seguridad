a859c08c86b6b48b8702c5a7cf668253d2b09c11
ismaelhtmljs <107575663+ismaelhtmljs@users.noreply.github.com>
2025-07-02 11:38:19 -0500

    test5

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="shortcut icon" href="icon.svg" type="image/x-icon" />
       </head>
+      {/*  */}
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
